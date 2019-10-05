9708e26f3af7dcd54f98940b1c19e18f872cbdce
GGuedesAB <gustavoguedesab@gmail.com>
2019-10-05 16:31:27 -0300

    Applying Victor's patch

--- src/database_setup/dataBaseDriver.py
+++ src/database_setup/dataBaseDriver.py
@@ -4,7 +4,7 @@ import logging
 from src.database_setup import accessControlUser
 from src.tools import logger
 
-class mysqlConnector ():
+class mysqlConnector:
 	def __init__ (self, data_base):
 		self.db_logger = logger.acsLogger()
 		self.db_logger.set_warning()
@@ -26,19 +26,22 @@ class mysqlConnector ():
 	# values must be a tuple in the same order as defined in query
 	def execute_query(self, query, values):
 		with self.conn.cursor() as cursor:
-			result = cursor.execute(query, values)
-			if type(result) is int:
-				self.conn.commit()
-				return result
+			try:
+				cursor.execute(query, values)
+			except pymysql.err.InternalError as internal_err:
+				self.db_logger.error (internal_err.args)
+			except TypeError as tperr:
+				self.db_logger.error (tperr.args)
+				return None
 			else:
-				result_return = result.fetchall()
+				query_result = cursor.fetchall()
 				self.conn.commit()
-				return result_return
-	
+				return query_result
+				
 	def __del__ (self):
 		self.conn.close()
 
-class dataBaseDriver ():
+class dataBaseDriver:
 
 	# checks if possible to connect to databse
 	def __init__(self, host, db_owner, password, db_name):
@@ -63,7 +66,7 @@ class dataBaseDriver ():
 
 	# groups must exist before inserting users
 	def define_new_group(self, acsgroup):
-		sql = "INSERT INTO `groups` (`number`,`description`) VALUES (%d,%s)"
+		sql = "INSERT INTO `groups` (`number`,`description`) VALUES (%s,%s)"
 		insert_tuple = (acsgroup.get_number(), acsgroup.get_description())
 		self.db_driver.execute_query(sql, insert_tuple)
 	
@@ -81,21 +84,23 @@ class dataBaseDriver ():
 
 	# link group and facility
 	def give_access(self, acsaccess):
-		sql = "INSERT INTO `access` (`group_number`,`facility_name`) VALUES (%d,%s)"
+		sql = "INSERT INTO `access` (`group_number`,`facility_name`) VALUES (%s,%s)"
 		insert_tuple = (acsaccess.get_group_number(),acsaccess.get_facility_name())
 		self.db_driver.execute_query(sql, insert_tuple)
 	
 	# user info
-	def retrieve_info_from_name (self, name):
-		sql = "SELECT `id`,`name`, `MAC`, `username`,`group_number` FROM `users` WHERE `name`=%s"
-		select_tuple = (name)
-		return self.db_driver.execute_query(sql, select_tuple)
+	def retrieve_info_from_username (self, username):
+		sql = "SELECT `id`,`name`, `MAC`, `username`,`group_number` FROM `users` WHERE `username`=%s"
+		select_tuple = (username)
+		result = self.db_driver.execute_query(sql, select_tuple)
+		return result
 
 	# check access to facilities from MAC
 	def check_access (self, MAC):
 		sql = "SELECT `facility_name` FROM `users`,`access` WHERE `users`.`group_number` = `access`.`group_number` AND `MAC`=%s"
 		select_tuple = (MAC)
-		return self.db_driver.execute_query(sql, select_tuple)
+		result = self.db_driver.execute_query(sql, select_tuple)
+		return result
 
 	# password must be already encrypted (bytes)
 	def add_user_info(self, acsuser):
@@ -105,26 +110,26 @@ class dataBaseDriver ():
 
 	# admin may edit any attribute
 	def edit_user(self, acsuser):
-		sql = "UPDATE `users` SET `id`=%d, `name`=%s, `MAC`=%s, `username`=%s,`password`=%s, group_number=%d where `MAC`=%s)"
+		sql = "UPDATE `users` SET `id`=%d, `name`=%s, `MAC`=%s, `username`=%s,`password`=%s, group_number=%s where `MAC`=%s)"
 		update_tuple = (acsuser.get_id(),acsuser.get_name(), acsuser.get_MAC(), acsuser.get_username(), acsuser.get_encrypted_password(), acsuser.get_group_number())
 		self.db_driver.execute_query(sql, update_tuple)
 
 	# admin can remove access from a group
 	def remove_access(self, acsaccess):
-		sql = "DELETE FROM `access` WHERE `group_number`=%d, `facility_name`=%s"
+		sql = "DELETE FROM `access` WHERE `group_number`=%s, `facility_name`=%s"
 		delete_tuple = (acsaccess.get_group_number(),acsaccess.get_facility_name())
 		self.db_driver.execute_query(sql, delete_tuple)
 
 	# admin can remove group
 	def remove_group(self, acsgroup):
-		sql = "DELETE FROM `groups` WHERE `number`=%d"
+		sql = "DELETE FROM `groups` WHERE `number`=%s"
 		delete_tuple = (acsgroup.get_number())
 		self.db_driver.execute_query(sql, delete_tuple)
 
 	# admin can remove user
 	def remove_user(self, acsuser):
 		sql = "DELETE FROM `users` WHERE `MAC`=%s OR `username=%s`"
-		delete_tuple = (acsuser.get_MAC(),acsuser.get_username)
+		delete_tuple = (acsuser.get_MAC(),acsuser.get_username())
 		self.db_driver.execute_query(sql, delete_tuple)
 
 	# admin can remove facility
@@ -135,12 +140,6 @@ class dataBaseDriver ():
 
 	# change group description
 	def change_group_description(self, acsgroup):
-		sql = "UPDATE `groups` SET `description`=%s where `number`=%d)"
+		sql = "UPDATE `groups` SET `description`=%s where `number`=%s)"
 		update_tuple = (acsgroup.get_description(),acsgroup.get_number())
 		self.db_driver.execute_query(sql, update_tuple)
-
-if __name__ == "__main__":
-	my_db = dataBaseDriver('localhost', 'root', 'jotaquest', 'accontrol')
-	new_user = accessControlUser.acsuser('Gustavo', 'myMACisTHIS', 'gustavob', 'Gatuno4268!', 2)
-	my_db.insert_new_user(new_user)
-	my_db.retrieve_info_from_name('Gustavo')
