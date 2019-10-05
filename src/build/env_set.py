c93e313bb60fc14b60352e9f6a10f8d3762b2a1d
GGuedesAB <gustavoguedesab@gmail.com>
2019-10-05 18:30:40 -0300

    Adding environment setup

---
+++ src/build/env_set.py
@@ -0,0 +1,44 @@
+import re
+import subprocess
+import os
+
+def setup_env ():
+    # Check if variable is not already set
+    dirname = re.match('(.*\/Access-Control-System)', os.getcwd())
+    dirname = dirname.group(1)
+    check_PYTHONPATH = "echo $PYTHONPATH"
+    is_bash = 'echo $SHELL'
+    restart_bash = '/bin/bash'
+    bashrc_env = 'echo \"export PYTHONPATH=' + dirname + '\" >> ~/.bashrc'
+
+    try:
+        is_bash = subprocess.run(is_bash, shell=True, check=True, stdout=subprocess.PIPE)
+    except subprocess.CalledProcessError as err:
+        print ('ERROR: ' + err.stderr.decode('utf-8'))
+        exit(1)
+    if is_bash.stdout.decode('utf-8') != '/bin/bash\n':
+        print ('ERROR: Please use bash as shell interpreter!')
+        exit (1)
+
+    try:
+        env_var_check = subprocess.run(check_PYTHONPATH, shell=True, check=True, stdout=subprocess.PIPE)
+    except subprocess.CalledProcessError as err:
+        print ('ERROR: ' + err.stderr.decode('utf-8'))
+        exit(1)
+    else:
+        is_set_PYTHONPATH = env_var_check.stdout.decode('utf-8')
+        if is_set_PYTHONPATH == "\n":
+            try:
+                subprocess.run(bashrc_env, shell=True, check=True)
+            except subprocess.CalledProcessError as err:
+                print ('ERROR: ' + err.stderr.decode('utf-8'))
+                exit(1)
+            try:
+                subprocess.run(restart_bash, shell=True, check=True)
+            except subprocess.CalledProcessError as err:
+                print ('ERROR: ' + err.stderr.decode('utf-8'))
+                exit(1)
+
+if __name__ == "__main__":
+    setup_env()
+    print ('\nEnvironment variables are set!\n')
