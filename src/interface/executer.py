2f786949304be9abd39929b66aeff64a8132824c
GGuedesAB <gustavoguedesab@gmail.com>
2019-10-22 23:18:35 -0300

    Fixing pretty print and making some minor changes to attatch GUI

--- src/interface/executer.py
+++ src/interface/executer.py
@@ -7,11 +7,17 @@ class executer:
     def __init__ (self, username, password):
         self.db_driver = dataBaseDriver.dataBaseDriver('localhost', username, password, 'accontrol')
 
-    def pretty_print (self, d, indent=2):
-        for element in d:
-            for item, value in element.items():
-                print (str(item) + ':' + str(value))
-            print ('\n')
+    def pretty_print (self, d):
+        try:
+            return_string = ''
+            for element in d:
+                for item, value in element.items():
+                    return_string += (str(item) + ': ' + str(value) + ' | ')
+                return_string += '\n\n'
+        except:
+            return 'Return from SQL querry is not printable.'
+        else:
+            return return_string
 
     def execute(self, current_user, command, args, command_table):
 
@@ -21,7 +27,12 @@ class executer:
             print ('EXECUTER: Command has execution errors.')
             return 1
 
-        if command_arg_type == 'self':
+        if command_arg_type == 'root':
+            if current_user.get_username() != 'root':
+                return 1
+            else:
+                db_command_args = None    
+        elif command_arg_type == 'self':
             db_command_args = current_user.get_MAC()
         elif command_arg_type == 'acsgroup':
             db_command_args = accessControlUser.acsgroup(args.get('number'), args.get('description'))
@@ -36,7 +47,10 @@ class executer:
             db_command_args = list(args.values())[0]
             
         try:
-            result = getattr(self.db_driver, command)(db_command_args)
+            if db_command_args is not None:
+                result = getattr(self.db_driver, command)(db_command_args)
+            else:
+                result = getattr(self.db_driver, command)()
         except ValueError as verr:
             print ('EXECUTER Value Error: ' + str(verr.args))
             return 1
@@ -44,6 +58,6 @@ class executer:
             print ('EXECUTER Type Error: ' + str(terr.args))
             return 1
         if result is not None :
-            self.pretty_print(result)
+            return self.pretty_print(result)
         
         
